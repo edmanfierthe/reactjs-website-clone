7596b0c239dd95f0b9f07f66582ba7f5ddf04841
edmanfierthe <fiertheedman14@yahoo.fr>
2023-05-28 14:33:20 -0500

    complete google authentication

--- realtor-clone/src/components/OAuth.jsx
+++ realtor-clone/src/components/OAuth.jsx
@@ -1,9 +1,41 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
 import React from 'react'
 import {FcGoogle} from "react-icons/fc"
-
+import { toast } from 'react-toastify'
+import { db } from '../firebase'
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore'
+import { useNavigate } from 'react-router'
 export default function OAuth() {
+
+  const navigate = useNavigate()
+
+  async function onGoogleClick(){
+    try {
+        const auth = getAuth()
+        const provider = new GoogleAuthProvider()
+        const result = await signInWithPopup(auth, provider)
+        const user = result.user
+
+        // Check if the user exists in the database
+
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+
+        if(!docSnap.exists()){
+          await setDoc(docRef, {
+            name: user.displayName,
+            email: user.email,
+            timestamp: serverTimestamp(),
+          });
+        }
+      navigate("/")
+    } catch (error) {
+        toast.error("Could not authenticate with Google")
+    }
+  }
+
   return (
-    <button className='flex items-center justify-center w-full bg-red-600
+    <button type='button' onClick={onGoogleClick} className='flex items-center justify-center w-full bg-red-600
     text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-700
     active:bg-red-800 shadow-md hover:shadow-lg active:shadow-lg transition 
     duration-150 ease-in-out rounded'>
