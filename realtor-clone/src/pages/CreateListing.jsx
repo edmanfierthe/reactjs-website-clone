e419372b43a2dcb8dad1464223a22e70d6611676
edmanfierthe <fiertheedman14@yahoo.fr>
2023-05-29 15:38:53 -0500

    Logic added for listing form

--- realtor-clone/src/pages/CreateListing.jsx
+++ realtor-clone/src/pages/CreateListing.jsx
@@ -1,7 +1,18 @@
 import { useState } from "react"
-
+import {toast} from "react-toastify"
+import Spinner from "../components/Spinner";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import {v4 as uuidv4} from "uuid";
+import {addDoc, collection, serverTimestamp} from "firebase/firestore"
+import {db} from "../firebase"
+import {useNavigate} from "react-router-dom"
 
 export default function CreateListing() {
+    const navigate = useNavigate();
+    const auth = getAuth();
+    const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+    const [loading, setLoading] = useState(false)
     const [formData, setFormData] = useState({
         type: "rent",
         name: "",
@@ -13,16 +24,146 @@ export default function CreateListing() {
         description: "",
         offer: false,
         regularPrice: 0,
-        discountPrice: 0,
+        discountedPrice: 0,
+        latitude: 0,
+        longitude:0,
+        images: {}
     });
-    const {type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountPrice} = formData;
-    function onChange(){}
+    const {type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, 
+        regularPrice, discountedPrice, latitude, longitude, images} = formData;
+    
+    function onChange(e){
+        let boolean = null;
+        if (e.target.value === "true"){
+            boolean = true;
+        }
+        if (e.target.value === "false"){
+            boolean = false;
+        }
+        // Checking files
+        if (e.target.files){
+            setFormData((prevState)=>({
+                ...prevState,
+                images: e.target.files
+            }))
+        }
+        // Boolean/Text/Number
+        if (!e.target.files){
+            setFormData((prevState)=>({
+                ...prevState,
+                [e.target.id]: boolean ?? e.target.value,
+            }));
+        }
+    }
+
+    async function onSubmit(e){
+        e.preventDefault();
+        setLoading(true)
+
+        //Toast error when discount < regular price
+        if(+discountedPrice >= +regularPrice){
+            setLoading(false)
+            toast.error("Discounted price needs to be less than regular price")
+            return;
+        }
+        // Toast error when max images > 6
+        if (images.length > 6){
+            setLoading(false)
+            toast.error("Maximum of images is 6")
+            return;
+        }
+
+        // Address is correct
+        let geolocation = {}
+        let location 
+        if (geolocationEnabled){
+            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`);
+            const data = await response.json();
+            console.log(data);
+            geolocation.lat  = data.results[0]?.geometry.location.lat ?? 0;
+            geolocation.lng  = data.results[0]?.geometry.location.lat ?? 0;
+
+            // Address is incorrect
+            location = data.status === "ZERO_RESULTS" && undefined;
+
+            if (location === undefined){
+                setLoading(false);
+                toast.error("Please enter a correct address");
+                return;
+            }
+        }else{
+            geolocation.lat = latitude;
+            geolocation.lng = longitude;
+        }
+
+        // Save Images to database
+        async function storeImage(image){
+            return new Promise((resolve, reject)=>{
+                const storage = getStorage()
+                const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+                const storageRef = ref(storage, filename);
+                const uploadTask = uploadBytesResumable(storageRef, image);
+                uploadTask.on('state_changed', 
+                    (snapshot) => {
+                        // Observe state change events such as progress, pause, and resume
+                        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            console.log('Upload is running');
+                            break;
+                        }
+                    }, 
+                    (error) => {
+                        // Handle unsuccessful uploads
+                    }, 
+                    () => {
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL);
+                        });
+                    }
+                    );
+            })
+        }
+        const imgUrls = await Promise.all(
+            [...images].map((image)=>storeImage(image))).catch((error)=>{
+                setLoading(false)
+                toast.error("Images not uploaded")
+                return;
+            });
+
+        const formDataCopy = {
+            ...formData,
+            imgUrls,
+            geolocation,
+            timestamp: serverTimestamp(),
+        };
+        delete formDataCopy.images;
+        !formDataCopy.offer && delete formDataCopy.discountedPrice;
+        delete formDataCopy.latitude;
+        delete formDataCopy.longitude;
+        const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+        setLoading(false);
+        toast.success("Listing Created");
+        navigate(`/category/${formDataCopy.type}/${docRef.id}`);
+    }    
+
+    if(loading){
+        return <Spinner />
+    }
+
   return (
     <main className="max-w-md px-2 mx-auto">
         <h1 className="text-3xl text-center mt-6 font-bold">
             Create a Listing
         </h1>
-        <form >
+        <form  onSubmit={onSubmit}>
             <p className="text-lg mt-6 font-semibold">Sell / Rent</p>
             <div className="flex">
                 <button type="button" id="type" value="sale" onClick={onChange} 
@@ -33,7 +174,7 @@ export default function CreateListing() {
                 }`}>
                     Sell
                 </button>
-                <button type="button" id="type" value="sale" onClick={onChange} 
+                <button type="button" id="type" value="rent" onClick={onChange} 
                 className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-medium rounded
                 hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration 
                 ease-in-out w-full ${
@@ -106,6 +247,26 @@ export default function CreateListing() {
             className="w-full px-4 py-2 text-xl text-gray-600 bg-white border border-gray-300
             rounded transition-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600
             mb-6" />
+            {!geolocationEnabled && (
+                <div className="flex space-x-6 justify-start mb-6">
+                    <div>
+                        <p className="text-lg font-semibold">Latitude</p>
+                        <input type="number" id="latitude" value={latitude} onChange={onChange} required
+                        min="-90" max="90" 
+                        className="w-full px-4 py-2 text-xl text-gray-700 bg-white border 
+                        border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white
+                        focus:text-gray-700 focus:border-slate-600 text-center"/>
+                    </div>
+                    <div>
+                        <p className="text-lg font-semibold">Longitude</p>
+                        <input type="number" id="longitude" value={longitude} onChange={onChange} required 
+                        min="-180" max="180"
+                        className="w-full px-4 py-2 text-xl text-gray-700 bg-white border 
+                        border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white
+                        focus:text-gray-700 focus:border-slate-600 text-center"/>
+                    </div>
+                </div>
+            )}
 
             <p className="text-lg font-semibold">Description</p>
             <textarea type="text" id="description" value={description} onChange={onChange} placeholder="description" required
@@ -137,7 +298,7 @@ export default function CreateListing() {
                     <p className="text-lg font-semibold">Regular Price</p>
                     <div className="flex w-full justify=center items-center space-x-6">
                         <input type="number" id="regularPrice" value={regularPrice} onChange={onChange} 
-                        min="50" max="40000000" required className="x-full px-4 py-2 text-xl text-gray-700 bg-white
+                        min="50" max="400000000" required className="x-full px-4 py-2 text-xl text-gray-700 bg-white
                         border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 
                         focus:bg-white focus:border-slate-600 text-center"/>
 
@@ -154,8 +315,8 @@ export default function CreateListing() {
                 <div>
                     <p className="text-lg font-semibold">Discounted Price</p>
                     <div className="flex w-full justify=center items-center space-x-6">
-                        <input type="number" id="discountedPrice" value={discountPrice} onChange={onChange} 
-                        min="50" max="40000000" required={offer} className="x-full px-4 py-2 text-xl text-gray-700 bg-white
+                        <input type="number" id="discountedPrice" value={discountedPrice} onChange={onChange} 
+                        min="50" max="400000000" required={offer} className="x-full px-4 py-2 text-xl text-gray-700 bg-white
                         border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 
                         focus:bg-white focus:border-slate-600 text-center"/>
 
