ae0a5d83898ee26387454dffbc95b55012acf141
edmanfierthe <fiertheedman14@yahoo.fr>
2023-05-29 11:16:27 -0500

    Part II create Listing finished

--- realtor-clone/src/pages/CreateListing.jsx
+++ realtor-clone/src/pages/CreateListing.jsx
@@ -13,8 +13,9 @@ export default function CreateListing() {
         description: "",
         offer: false,
         regularPrice: 0,
+        discountPrice: 0,
     });
-    const {type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice} = formData;
+    const {type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountPrice} = formData;
     function onChange(){}
   return (
     <main className="max-w-md px-2 mx-auto">
@@ -148,6 +149,37 @@ export default function CreateListing() {
                     </div>
                 </div>
             </div>
+            {offer && (
+                <div className="flex items-center mb-6">
+                <div>
+                    <p className="text-lg font-semibold">Discounted Price</p>
+                    <div className="flex w-full justify=center items-center space-x-6">
+                        <input type="number" id="discountedPrice" value={discountPrice} onChange={onChange} 
+                        min="50" max="40000000" required={offer} className="x-full px-4 py-2 text-xl text-gray-700 bg-white
+                        border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 
+                        focus:bg-white focus:border-slate-600 text-center"/>
+
+                        {type === "rent" && (
+                        <div>
+                            <p className="text-md w-full whitespace-nowrap">$ / Month</p>
+                        </div>
+                    )}
+                    </div>
+                </div>
+            </div>
+            )}
+            <div className="mb-6">
+                <p className="text-lg font-semibold">Images</p>
+                <p className="text-gray-600">The first image will be the cover (max 6)</p>
+                <input type="file" id="images" onChange={onChange} accept=".jpg, .png, .jpeg"
+                multiple required className="w-full px-3 py-1.5 text-gray-700 bg-white border 
+                border-gray-600 rounded transition duration-150 ease-in-out
+                focus:bg-white focus:border-slate-600 "/>
+            </div>
+            <button type="submit" className="mb-6 w-full px-7 py-3 bg-blue-600 text-white
+            font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700
+            hover:shadow-lg focus:bg-blue-700 focus:shadow-lg active:bg-blue-800
+            active:shadow-lg transition duration-150 ease-in-out">Create Listing</button>
         </form>
     </main>
   )
