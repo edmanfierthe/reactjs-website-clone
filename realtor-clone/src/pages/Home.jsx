fe85d08bb4ade4edf2fb888bb003a79d07125dfe
edmanfierthe <fiertheedman14@yahoo.fr>
2023-06-01 00:32:11 -0500

    Google maps and Home Slider

--- realtor-clone/src/pages/Home.jsx
+++ realtor-clone/src/pages/Home.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Slider from '../components/Slider'
+import { collection, getDoc, getDocs, limit, orderBy, query } from 'firebase/firestore';
+import { db } from '../firebase';
 
 export default function Home() {
+
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(()=>{
+    async function fetchListings(){
+      const listingsRef = collection(db, "listings");
+      const q = query(listingsRef, orderBy("timestamp", "desc"), limit(5));
+      const querySnap = await getDocs(q)
+      let listings = [];
+      querySnap.forEach((doc)=>{
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchListings();
+  },[])
   return (
-    <div>Home</div>
+    <div>
+      <Slider />
+    </div>
   )
 }
